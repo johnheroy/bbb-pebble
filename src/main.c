9731acf71cf5f6887d58abf5ebebe261966e95a6
John Heroy <johnheroy@users.noreply.github.com>
2015-08-23 18:09:52 -0700

    Basic (albeit ugly) navigation

--- src/main.c
+++ src/main.c
@@ -12,6 +12,8 @@
 #endif
 
 static Window *s_main_window;
+static Window *loading_window;
+static TextLayer *loading_text;
 static GFont s_res_gothic_28_bold;
 static TextLayer *title_text_layer;
 static TextLayer *instructions_text_layer;
@@ -51,6 +53,32 @@ static void main_window_unload(Window *window) {
   window_destroy(window);
 }
 
+static void loading_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  loading_text = text_layer_create(GRect(42, 12, 60, 100));
+  text_layer_set_text(loading_text, "Loading");
+  
+  layer_add_child(window_layer, text_layer_get_layer(loading_text));
+}
+
+static void loading_window_unload(Window *window) {
+  window_destroy(window);
+}
+
+void select_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+  loading_window = window_create();
+  window_set_window_handlers(loading_window, (WindowHandlers) {
+    .load = loading_window_load,
+    .unload = loading_window_unload
+  });
+  window_stack_push(loading_window, true);
+}
+
+void config_provider(Window *window) {
+ // single click / repeat-on-hold config:
+  window_single_repeating_click_subscribe(BUTTON_ID_SELECT, 1000, select_single_click_handler);
+}
+
 static void init() {
   // Create main Window
   s_main_window = window_create();
@@ -59,6 +87,7 @@ static void init() {
     .load = main_window_load,
     .unload = main_window_unload,
   });
+  window_set_click_config_provider(s_main_window, (ClickConfigProvider) config_provider);
   window_stack_push(s_main_window, true);
 }
 
